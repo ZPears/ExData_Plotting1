3921525e8b3f649dc7d33f4d7b4f8c1d6e0be274
Zach <zachary.p.pearson@gmail.com>
2015-05-10 08:13:09 -0500

    solved problem #2

--- answers/plot1.R
+++ answers/plot1.R
@@ -1,6 +1,15 @@
-data <- read.table(unz('exdata_data_household_power_consumption.zip', 'household_power_consumption.txt'), header=TRUE, sep=";", colClasses=c('character', 'character', rep('numeric', 7)), na.strings='?')
+#package for getZip()
+require("Hmisc")
+#load in and format dates
+data <- read.table(getZip("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"), header=TRUE, sep=";", colClasses=c('character', 'character', rep('numeric', 7)), na.strings='?')
 data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+#select sepecific dates
 data <- data[(data$Date == '2007-02-01' | data$Date == '2007-02-02'),]
-png(file="plot1.png")
+#format times
+data$Time <- paste(as.character(data$Date), data$Time)
+data$Time <- strptime(data$Time, format="%Y-%m-%d %H:%M:%S")
+
+#generate plot
+png(file="answers/plot1.png")
 hist(data$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", col="red")
 dev.off()
---
+++ answers/plot2.R
@@ -0,0 +1,15 @@
+#package for getZip()
+require("Hmisc")
+#load in and format dates
+data <- read.table(getZip("http://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"), header=TRUE, sep=";", colClasses=c('character', 'character', rep('numeric', 7)), na.strings='?')
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+#select sepecific dates
+data <- data[(data$Date == '2007-02-01' | data$Date == '2007-02-02'),]
+#format times
+data$Time <- paste(as.character(data$Date), data$Time)
+data$Time <- strptime(data$Time, format="%Y-%m-%d %H:%M:%S")
+
+#generate plot
+png(file="answers/plot2.png")
+with(data, plot(Time, Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)"))
+dev.off()
